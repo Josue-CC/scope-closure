9cdb9b09cb96e2f82e1ba2ff05807b8506dd4608
Jose Curi <curicurilla.jose123@gmail.com>
2024-01-01 22:13:59 -0500

    Function Scope

---
+++ src/scope/function.js
@@ -0,0 +1,15 @@
+function greeting(){
+    let userName = 'keila';
+   console.log(userName);
+
+    if (userName === 'keila') {
+        console.log(`Hello ${userName} !`);
+    }else{
+        console.log('error');
+    }
+}
+greeting();
+
+console.log(userName);
+
+// Funcion scope
