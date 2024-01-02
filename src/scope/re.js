c8fc8cece1020f53ef10b0e3ad4b409be1b07558
Jose Curi <curicurilla.jose123@gmail.com>
2024-01-01 22:47:10 -0500

    estricto

---
+++ src/scope/re.js
@@ -0,0 +1,33 @@
+var firstName ; // esta variable se asigna undefine
+
+firstName = 'Jose';
+
+console.log(firstName);
+
+var lastName = 'Curi'; // Declarando / asignando
+lastName = 'Curilla'; // reasignando
+console.log(lastName);
+
+var secondName = 'Karen'; //declaramos / asignamos
+var secondName = 'Ana'; // reasignamos
+console.log(secondName);
+
+let fruit = 'Apple'// declarando / asignamos
+fruit = 'Kiwi' //reasignar
+
+//let fruit = 'Banana'; // con let no se puede redeclarar
+
+
+// const 
+
+const animal = 'dog'; // declaras y asignar
+animal = 'cat'; //reasignar  / No se puede
+//const animal = 'snake'; // nose puede
+console.log(animal);
+
+
+const vehiculos = [];
+vehiculos.push('carrito');
+console.log(vehiculos);
+vehiculos.pop();
+console.log(vehiculos);
---
+++ src/scope/strics.js
@@ -0,0 +1,12 @@
+'use strict';
+//reclas especiales que se dice para cumplir los estandares 
+pi = 3.1416; // asigno el valor
+
+console.log(pi);
+
+
+function myFunction(){
+    //'use strict';
+    return pi = 3.1416
+}
+console.log(myFunction())
